8412d4b6875d526745a1fd22f38d0c22cb6c0971
VitalKr <vitalkr@mail.ru>
2022-04-19 11:24:53 +0300

    Переделка new

--- src/main/kotlin/1_3_1.kt
+++ src/main/kotlin/1_3_1.kt
@@ -1,5 +1,6 @@
+const val SECUNDA: Int = 60
 const val MINUTE: Int = 60
-const val HOUR: Int = MINUTE * MINUTE
+const val HOUR: Int = MINUTE * SECUNDA
 const val DAY: Int = HOUR * 24
 const val TWO_DAYS: Int = DAY * 2
 const val TREE_DAYS: Int = DAY * 3
@@ -9,27 +10,33 @@ fun main() {
     do {
         println()
         println("Введите время проведенное в сети в секундах или \"end\" для выхода.")
-        val timeUser = readln()
-        if (isNotEnd(timeUser)) {
+        val inputUser = readln()
+        if (isNotEnd(inputUser)) {
             try {
-                time = timeUser.toInt()
+                time = inputUser.toInt()
             } catch (e: Exception) {
                 println("ВНИМАНИЕ!!! Вы ввели не число!")
                 continue
             }
 
-            println(agoToText(time, time / MINUTE, time / MINUTE / MINUTE))
+            println(agoToText(time))
         }
-    } while (isNotEnd(timeUser))
+    } while (isNotEnd(inputUser))
 }
 
 private fun isNotEnd(timeUser: String) = timeUser != "end"
 
-private fun agoToText(time: Int, timeMin: Int, timeHour: Int): String =
+private fun agoToText(time: Int): String =
     when (time) {
         in 1..MINUTE -> "был(а) только что"
-        in (MINUTE + 1)..HOUR -> "был(а) $timeMin ${nameNumMin(time / MINUTE)} назад"
-        in (HOUR + 1)..DAY -> "был(а) в сети $timeHour ${nameNumHour(time / MINUTE / MINUTE)} назад"
+        in (MINUTE + 1)..HOUR -> {
+            val timeMin = time / MINUTE
+            "был(а) $timeMin ${nameNumMin(timeMin)} назад"
+        }
+        in (HOUR + 1)..DAY -> {
+            val timeHour = time / MINUTE / SECUNDA
+            "был(а) в сети $timeHour ${nameNumHour(timeHour)} назад"
+        }
         in (DAY + 1)..TWO_DAYS -> "был(а) в сети сегодня"
         in (TWO_DAYS + 1)..TREE_DAYS -> "был(а) в сети вчера"
         in (TREE_DAYS + 1)..Int.MAX_VALUE -> "был(а) в сети давно"
@@ -42,12 +49,14 @@ fun nameNumMin(timeMin: Int) = nameNum(
     few = "минуты",
     many = "минут"
 )
+
 fun nameNumHour(timeHour: Int) = nameNum(
     num = timeHour,
     one = "час",
     few = "часа",
     many = "часов"
 )
+
 fun nameNum(num: Int, one: String, few: String, many: String): String {
     val remain10 = num % 10
     val remain100 = num % 100
